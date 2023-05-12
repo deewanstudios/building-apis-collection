37126ecea7d93f8e866643b2aaa3d0935b01a3ba
Adedayo Adedapo <adedayo.adedapo@airangel.com>
2023-05-12 17:21:56 +0100

    Updated index file to include calls for all http reques verb types

--- JsonStore/client/index.php
+++ JsonStore/client/index.php
@@ -23,18 +23,23 @@ require __DIR__ . '/vendor/autoload.php'; ?>
     <div class="text-center text-bold"></div>
     <?php
     $client = new \GuzzleHttp\Client();
+
+    $request_method = ['get' => 'GET', 'post' => 'POST', 'put' => 'PUT', 'patch' => 'PATCH', 'delete' => 'DELETE'];
+    $collection_id = '21a647e5-5500-407c-b556-911279b9084d';
+    $access_token = 'fa7a8257-ee3a-42a4-bf97-43d3e9341cbb';
+    $endpoint = 'https://api.myjson.online/v1/records';
+
+
     /* Call to make GET request to my JSON Store */
     /*
-$json = $client->request('GET', 'https://api.myjson.online/v1/records/e9db01eb-11b9-4206-aef8-082e15657796');
-echo$json->getBody();
- */
+    $json = $client->request('GET', $endpoint.'/e9db01eb-11b9-4206-aef8-082e15657796');
+    echo$json->getBody();
+    */
 
     /* Call to make POST request to JSON Store  */
-    $body = '{"name":"Michelangelo ", "type":"Turtle", "age":35}';
-    $collection_id = '21a647e5-5500-407c-b556-911279b9084d';
-    $request_method = 'POST';
-    $access_token = 'fa7a8257-ee3a-42a4-bf97-43d3e9341cbb';
-    $options =    [
+    /* 
+    $body = '{"name":"SuperTed", "type":"Bear", "age":55}';
+    $post_options = [
         'headers' => [
             'Content-Type' => 'application/x-www-form-urlencoded',
             'x-collection-access-token' => $access_token
@@ -44,15 +49,68 @@ echo$json->getBody();
             'jsonData' => $body,
             'collectionId' => $collection_id
         ]
-    ];
-    $endpoint = 'https://api.myjson.online/v1/records';
+    ]; 
+    */
+    /* Call to make PUT request to JSON Store  */
+
+    /* $body = '{"name":"Garfield", "type":"Immortal", "age":39}';
+    $uri_id = '/50966e31-6670-4eb5-b179-27f704926a99';
+    $put_options = [
+        'headers' => [
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'x-collection-access-token' => $access_token
+        ],
+        'allow_redirects' => true,
+        'form_params' => [
+            'jsonData' => $body,
+            'collectionId' => $collection_id
+        ]
+    ]; */ 
+    /* Call to make PATCH request to JSON Store  */
+
+   /*  $body = '{"age":45}';
+    $uri_id = '/50966e31-6670-4eb5-b179-27f704926a99';
+    $patch_options = [
+        'headers' => [
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'x-collection-access-token' => $access_token
+        ],
+        'allow_redirects' => true,
+        'form_params' => [
+            'jsonData' => $body,
+            // 'collectionId' => $collection_id
+        ]
+    ];  */
+    /* Call to make DELETE request to JSON Store  */
+
+    $uri_id = '/e9db01eb-11b9-4206-aef8-082e15657796';
+    $delete_options = [
+        'headers' => [
+            'x-collection-access-token' => $access_token
+        ],
+        'allow_redirects' => true,
+        'body'=>'Deleted'
+        /* 'form_params' => [
+            'jsonData' => $body,
+            // 'collectionId' => $collection_id
+        ] */
+    ]; 
     try {
         $response = $client->request(
-            $request_method,
-            $endpoint,
-            $options
+            $request_method['delete'],
+            $endpoint.$uri_id,
+            $delete_options
         );
         if ($response->getStatusCode() == 200) {
+            // $obj = json_decode($response->getBody());
+           /*  $uri = $endpoint . '/' . $obj->id;
+            $get_options = [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'x-collection-access-token' => $access_token
+                ]
+            ];
+            $json = $client->request('GET', $uri, $get_options); */
             echo '<div class"">';
             echo $response->getBody();
             echo '</div>';
